7f7e38c1261bbada90495e5c8f7c6353a57032f6
Anthony Cook <anthonymichaelcook@gmail.com>
2012-03-13 10:54:49 -0400

    Setup install.sh to ask user to provide the name and email they want in their gitconfig.

--- install.sh
+++ install.sh
@@ -6,6 +6,35 @@
 # Put all the dotfiles in the same directory as the install script and list them in the file variable.
 # run with `./install.sh`
 
+while getopts ":n" opt; do
+  case $opt in
+    n)
+      $noninteractive = 'true'
+      ;;
+    \?)
+      echo "Ignoring invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+
+if [[ $1 != 'true' ]]; then
+  echo "Enter additional info to use for this configuration (pass -n to skip this in the future)."
+  echo -ne "git name: "
+  read $git_name
+  echo -ne "git email: "
+  read $git_email
+  echo -ne "github username: "
+  read $github_username
+  echo -ne "github token: "
+  read $github_token
+fi
+
+git config --global user.name $git_name > /dev/null
+git config --global user.email $git_email > /dev/null
+git config --global github.user $github_username > /dev/null
+git config --global github.token $github_token > /dev/null
+
 cd `dirname $0`
 
 dir=`pwd`
