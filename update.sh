a7b7c233624ddea13dae1eba1eff9aad868e08c2
Anthony Cook <anthonymichaelcook@gmail.com>
2012-04-17 17:26:52 -0400

    Pulled in changes for update.sh from CDEI.

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
+cd `dirname $0`
 echo Updating remote submodules...
 git submodule update --init --recursive
-git submodule foreach "git co master"
-git submodule foreach "git pull"
+git submodule foreach "git fetch && git checkout origin/HEAD"
