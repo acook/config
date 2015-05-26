3e3ed2eda218b60e5919424aa7941d7140b5354d
Anthony Cook <anthonymichaelcook@gmail.com>
2015-05-26 03:30:15 -0500

    Whitespace paragraphing.

--- update.bash
+++ update.bash
@@ -3,19 +3,25 @@
 cd `dirname $0`
 git diff-index --quiet HEAD --
 git_dirty=$?
+
 if [ $git_dirty -ne 0 ]; then
   echo Stashing local changes...
   git stash
 fi
+
 echo Updating local repo...
 git pull --rebase origin master
+
 echo Updating remote submodules...
 git submodule update --init --recursive
 git submodule foreach "git fetch -q && git checkout -q origin/HEAD"
+
 if [ $git_dirty -ne 0 ]; then
   echo Restoring local changes...
   git unstash
 fi
+
 echo Updating Vim plugins...
 vim -e +PlugUpgrade +PlugUpdate +qa! 2> /dev/null
+
 echo Done!
