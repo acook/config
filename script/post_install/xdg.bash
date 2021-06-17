df23c7bab78681b126a001c7a02dc4f57245ef7c
Anthony M. Cook <anthony.cook@valimail.com>
2021-06-17 18:09:57 -0500

    Use general xdg setup instead of fish-specific setup

---
+++ script/post_install/xdg.bash
@@ -0,0 +1,19 @@
+# sets up the ~/.config and ~/.local paths
+
+function post_install {
+
+  local shared_backup_dir="$backup_dir"
+
+  local source_dir="$dir/xdg_conf"
+  local target_dir="$HOME/.config"
+  local backup_dir="$shared_backup_dir/.config"
+
+  backup_and_link_directory "$filename" "$source_dir" "$target_dir" "$backup_dir"
+
+  local source_dir="$dir/xdg_data"
+  local target_dir="$HOME/.local"
+  local backup_dir="$shared_backup_dir/.local"
+
+  backup_and_link_directory "$filename" "$source_dir" "$target_dir" "$backup_dir"
+
+}
