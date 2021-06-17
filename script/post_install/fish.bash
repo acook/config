071911acc6a4fba254d4124d2f33a63554bb52fc
Anthony Cook <github@anthonymcook.com>
2021-06-17 17:00:01 -0500

    post_install: fish was broken, making it generic

--- script/post_install/fish.bash
+++ script/post_install/fish.bash
@@ -3,16 +3,15 @@
 function post_install {
   local filename=fish
   local source_dir="$dot_dir"
-  local target_dir="$HOME"
+  local target_dir="$HOME/.config"
 
+  mkdir -p "$target_dir"
   cd "$target_dir"
-  mkdir .config
-  cd .config
 
-  if symlink_exists $target_dir/fish; then
+  if symlink_exists $target_dir/$filename; then
     echo -ne "rm: symlink "
-    rm -v "$target_dir/fish"
+    rm -v "$target_dir/$filename"
   fi
 
-  backup_and_link $filename "$source_dir" "$target_dir" "$backup_dir"
+  backup_and_link "$filename" "$source_dir" "$target_dir" "$backup_dir"
 }
