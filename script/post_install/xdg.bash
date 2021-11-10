61b6e9569276bd240c2ccc8b1726480f676f9b1b
Anthony M. Cook <github@anthonymcook.com>
2021-11-09 19:13:59 -0600

    Use XDG environment variables if available

--- script/post_install/xdg.bash
+++ script/post_install/xdg.bash
@@ -5,14 +5,14 @@ function post_install {
   local shared_backup_dir="$backup_dir"
 
   local source_dir="$dir/xdg_conf"
-  local target_dir="$HOME/.config"
+  local target_dir="${XDG_CONFIG_HOME:-$HOME/.config}"
   local backup_dir="$shared_backup_dir/.config"
 
   backup_and_link_directory "$source_dir" "$target_dir" "$backup_dir"
 
   local source_dir="$dir/xdg_data"
-  local target_dir="$HOME/.local"
-  local backup_dir="$shared_backup_dir/.local"
+  local target_dir="${XDG_DATA_HOME:-$HOME/.local/share}"
+  local backup_dir="$shared_backup_dir/.local/share"
 
   backup_and_link_directory "$source_dir" "$target_dir" "$backup_dir"
 
