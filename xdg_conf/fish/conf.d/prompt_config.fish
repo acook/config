987fc0b560b5b2b00ac6ffac292211261accd47e
Anthony Cook <github@anthonymcook.com>
2023-02-23 16:03:14 -0600

    Move prompt config into its own fish file, load local config if available

---
+++ xdg_conf/fish/conf.d/prompt_config.fish
@@ -0,0 +1,24 @@
+# for hydro prompt
+set hydro_color_error ff00af
+set hydro_color_git ff00af
+set hydro_color_prompt afff00
+
+# for pure prompt
+set -g async_prompt_functions _pure_prompt_git
+set pure_color_danger #ff00af
+set pure_color_primary #afff00
+set pure_color_success #1f51ff
+#FF6700
+
+# for tide prompt
+set tide_status_color 1f51ff
+set tide_status_bg_color_failure ff00af
+set tide_status_color_failure white
+set tide_pwd_bg_color afff00
+set tide_pwd_color_anchors 000000
+set tide_pwd_color_dirs black
+set tide_pwd_color_truncated_dirs aa8888
+set tide_git_bg_color_urgent ff00af
+set tide_git_bg_color 1f51ff
+set tide_git_bg_color_unstable FF6700
+
--- xdg_conf/fish/config.fish
+++ xdg_conf/fish/config.fish
@@ -64,27 +64,14 @@ function rg
   command rg --no-messages $argv
 end
 
-# for hydro prompt
-set hydro_color_error ff00af
-set hydro_color_git ff00af
-set hydro_color_prompt afff00
+# helpers
 
-# for pure prompt
-set -g async_prompt_functions _pure_prompt_git
-set pure_color_danger #ff00af
-set pure_color_primary #afff00
-set pure_color_success #1f51ff
-#FF6700
+function setdefault --no-scope-shadowing
+    set -q $argv[1] || set $argv[1] $argv[2..-1]
+end
 
-# for tide prompt
-set tide_status_color 1f51ff
-set tide_status_bg_color_failure ff00af
-set tide_status_color_failure white
-set tide_pwd_bg_color afff00
-set tide_pwd_color_anchors 000000
-set tide_pwd_color_dirs black
-set tide_pwd_color_truncated_dirs aa8888
-set tide_git_bg_color_urgent ff00af
-set tide_git_bg_color 1f51ff
-set tide_git_bg_color_unstable FF6700
+setdefault LOCAL_FISH_CONFIG ~/.local.config.fish
 
+if test -e $LOCAL_FISH_CONFIG
+  source $LOCAL_FISH_CONFIG
+end
