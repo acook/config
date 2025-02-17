38560ba9b240bd632df9800d598be4c1696aa21b
Anthony Cook <github@anthonymcook.com>
2025-02-17 14:15:52 -0600

    fish: add fzf plugin for fine fuzzy finding functions

---
+++ xdg_conf/fish/conf.user.d/fzf.fish
@@ -0,0 +1,14 @@
+
+
+# For https://github.com/PatrickF1/fzf.fish
+
+# Delete the fzf plugin's ctrl-r replacement, I like the default one better.
+# Ideally, I would modify fzf to behave closer to the default one,
+# but I don't know if I can make it properly syntax hilite commands or not.
+#
+# This makes the fzf version available on alt+ctrl+r instead.
+
+if type -q fzf_configure_bindings
+  fzf_configure_bindings --history=\e\cr
+end
+
