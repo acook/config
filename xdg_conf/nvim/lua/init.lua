08d2fdfad7a02232ccf272bfbbb7aa7572cca405
Anthony Cook <github@anthonymcook.com>
2025-10-11 05:45:55 -0500

    neovim: configure diagnostics

--- xdg_conf/nvim/lua/init.lua
+++ xdg_conf/nvim/lua/init.lua
@@ -1,4 +1,21 @@
+-- for NeoVim configuration which is better done using Lua
 local xei = require 'xei'
+
+-- configure NeoVim's builtin diagnostics
+vim.diagnostic.config{
+    -- only show virtual text for the current line
+    virtual_text = { current_line = true },
+    underline = true,
+    -- change indicator signs to nicer nerd font ones
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN]  = "",
+            [vim.diagnostic.severity.INFO]  = "",
+            [vim.diagnostic.severity.HINT]  = "",
+        },
+    },
+}
 
 -- set the color of the window pane separator
 local dim, _ = xei.hlhl({ {'SignColumn', 'bg'}, {'CursorLine','bg'}, {'Conceal', 'fg'} })
